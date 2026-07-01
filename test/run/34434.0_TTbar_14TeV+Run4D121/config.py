ebb272efa4e6e31ee8a482d9cb965cc6e0b3da85
Noor Faqihi <noor.faqihi@cern.ch>
2026-07-01 15:49:04 +0200

    Modify Producer types

--- test/run/34434.0_TTbar_14TeV+Run4D121/config.py
+++ test/run/34434.0_TTbar_14TeV+Run4D121/config.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from step3_RAW2DIGI_RECO_RECOSIM_PAT_VALIDATION_DQM import process
 #If you have an EDAnalyzer, you can change it to that too 
 process.ACTSTrackingGeometryProducer_Phase2 = cms.EDProducer("ACTSTrackingGeometryProducer_Phase2") #Change MyProducer to the name of the producer 
-
+                                                   #It was an EDProducer 
 process.path = cms.Path(process.ACTSTrackingGeometryProducer_Phase2)
 
 process.schedule = cms.Schedule(process.path)
