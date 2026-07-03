c1b3db3d28f8f1111515fddcf61c4d0f914bc11c
Noor Faqihi <noor.faqihi@cern.ch>
2026-07-03 17:29:53 +0200

    Add the EDAnalyzer to the cfg file

--- test/run/34434.0_TTbar_14TeV+Run4D121/config.py
+++ test/run/34434.0_TTbar_14TeV+Run4D121/config.py
@@ -2,10 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 from step3_RAW2DIGI_RECO_RECOSIM_PAT_VALIDATION_DQM import process #This performs step3 only, not 1,2 and 3 
 #If you have an EDAnalyzer, you can change it to that too 
+
+#ACTSTrackingGeometryProducer_Phase2::ESProducer 
 process.ACTSTrackingGeometryProducer_Phase2 = cms.ESProducer("ACTSTrackingGeometryProducer_Phase2") #Change MyProducer to the name of the producer 
-             
-#process.ACTSTrackingGeometryProducer_Phase2 = cms.EDAnalyzer("ACTSTrackingGeometryProducer_Phase2") change into the name of the analyzer 
-#process.path = cms.Path(process.ACTSTrackingGeometryProducer_Phase2) add analyzer name 
 
 process.ACTSTrackingGeometryProducer_Phase2 = cms.ESProducer(
     "ACTSTrackingGeometryProducer_Phase2",
@@ -20,4 +19,9 @@ process.ACTSTrackingGeometryProducer_Phase2 = cms.ESProducer(
 )
 # Acts::RangeXD<2, double, std::array> Range({0, 0}, {0, 0});
 
-#process.schedule = cms.Schedule(process.path)
+
+#GeometryDumper_Phase2::EDAnalyzer           
+process.ACTSTrackingGeometryProducer_Phase2 = cms.EDAnalyzer("GeometryDumper_Phase2") #change into the name of the analyzer 
+process.path = cms.Path(process.GeometryDumper_Phase2) #add analyzer name 
+
+process.schedule = cms.Schedule(process.path)
