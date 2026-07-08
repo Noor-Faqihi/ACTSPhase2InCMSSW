56770506374f59b6b84b05fd27127ca2e20e9215
Noor Faqihi <noor.faqihi@cern.ch>
2026-07-08 13:47:17 +0200

    Config without EDAnalyzer

--- test/run/34434.0_TTbar_14TeV+Run4D121/config.py
+++ test/run/34434.0_TTbar_14TeV+Run4D121/config.py
@@ -8,7 +8,7 @@ process.ACTSTrackingGeometryProducer_Phase2 = cms.ESProducer("ACTSTrackingGeomet
 
 process.ACTSTrackingGeometryProducer_Phase2 = cms.ESProducer(
     "ACTSTrackingGeometryProducer_Phase2",
-    saveObjfile = cms.untracked.bool(True), 
+    saveObjfile = cms.untracked.bool(False), 
     saveSvgfile = cms.untracked.bool(False),
     mapMaterial = cms.untracked.bool(False),
     outputObjFile = cms.untracked.string("outputObjFile"),
@@ -20,8 +20,8 @@ process.ACTSTrackingGeometryProducer_Phase2 = cms.ESProducer(
 # Acts::RangeXD<2, double, std::array> Range({0, 0}, {0, 0});
 
 
-#GeometryDumper_Phase2::EDAnalyzer           
-process.ACTSTrackingGeometryProducer_Phase2 = cms.EDAnalyzer("GeometryDumper_Phase2") #change into the name of the analyzer 
+""" #GeometryDumper_Phase2::EDAnalyzer           
+process.GeometryDumper_Phase2 = cms.EDAnalyzer("GeometryDumper_Phase2") #change into the name of the analyzer 
 process.path = cms.Path(process.GeometryDumper_Phase2) #add analyzer name 
 
-process.schedule = cms.Schedule(process.path)
+process.schedule = cms.Schedule(process.path) """
